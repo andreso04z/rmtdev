83a01f2673a9c50e2a2cd8cceedaaef63509c29e
andreso04z <andresdavidobon@gmail.com>
2025-11-09 18:39:50 -0500

    Set up markup and styles starter template.

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState("");
+
   return (
-    <form action="#" className="search">
+    <form
+      action="#"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      className="search"
+    >
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
         spellCheck="false"
         type="text"
         required
